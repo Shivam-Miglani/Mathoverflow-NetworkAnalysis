cad98966351104666682b3adfe27087658719f03
Shivam Miglani <shivam@Shivams-MacBook-Air.local>
2018-04-20 09:45:44 +0200

    New dataset with 3846 nodes.

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -4,3 +4,13 @@ import numpy as np
 df = pd.read_csv('data/tp1/sx-mathoverflow-c2q-tp1.txt',names=['u', 'v', 'ts'],header=None,sep=' ')
 
 print(df.u.unique().size)
+
+
+df2 = pd.read_csv('data/tp2/sx-mathoverflow-a2q-tp2.txt',names=['u', 'v', 'ts'],header=None,sep=' ')
+
+print(df.u.unique().size)
+
+
+df3 = pd.read_csv('data/tp2/sx-mathoverflow-c2a-tp2.txt',names=['u', 'v', 'ts'],header=None,sep=' ')
+
+print(df.u.unique().size)
